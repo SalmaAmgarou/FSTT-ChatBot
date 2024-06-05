3907d3d016b65847a42935f06875ad3eee332787
SalmaAmgarou <118823594+SalmaAmgarou@users.noreply.github.com>
2024-06-05 13:12:47 +0100

    Update App.js

--- chatbot-app/src/App.js
+++ chatbot-app/src/App.js
@@ -1,14 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import './index.css';
-import { FiAlignJustify } from "react-icons/fi";
-import { FiEdit } from "react-icons/fi";
-import { FiSlack } from "react-icons/fi";
-import { FcDecision } from "react-icons/fc";
-import { FcDribbble } from "react-icons/fc";
-import { FcLink } from "react-icons/fc";
-import { FcBinoculars } from "react-icons/fc";
-import { FcLightAtTheEndOfTunnel } from "react-icons/fc";
+import { FiAlignJustify, FiEdit, FiSlack } from "react-icons/fi";
+import { FcDecision, FcDribbble, FcLink, FcBinoculars, FcLightAtTheEndOfTunnel } from "react-icons/fc";
 
 function App() {
     const [query, setQuery] = useState('');
@@ -21,7 +15,6 @@ function App() {
         "Y a-t-il des conférences prévues ce mois-ci ?",
         "Comment puis-je accéder à la bibliothèque de la faculté ?",
         "Y a-t-il des clubs ou des associations étudiantes ?",
-
     ]);
     const messagesEndRef = useRef(null);
 
@@ -52,9 +45,7 @@ function App() {
     
         try {
             const res = await axios.post('http://localhost:5000/query', { query });
-            // Extract the response part from the model's answer
-            const responseText = res.data.response.split('✅Réponse :')[1].trim();
-            // Add response to messages
+            const responseText = res.data.response;
             setMessages((prevMessages) => [...prevMessages, { sender: 'bot', text: responseText }]);
         } catch (error) {
             console.error('Error querying model:', error);
@@ -68,21 +59,22 @@ function App() {
     };
 
     const handleNewChat = () => {
-        // Clear messages and query
         setMessages([]);
         setQuery('');
     };
 
     const handleSuggestionClick = async (suggestion) => {
         try {
-            // Add user query to messages
             setMessages((prevMessages) => [...prevMessages, { sender: 'user', text: suggestion }]);
             setSuggestions([]); // Remove all suggestions
             const res = await axios.post('http://localhost:5000/query', { query: suggestion });
-            // Extract the response part from the model's answer
-            const responseText = res.data.response.split('✅Réponse :')[1].trim();
-            // Add response to messages
-            setMessages((prevMessages) => [...prevMessages, { sender: 'bot', text: responseText }]);
+            
+            // Assuming the response contains the formatted response, extract the answer
+            const responseText = res.data.response;
+            const answerMatch = responseText.match(/✅Réponse :(.+)/);
+            const answerText = answerMatch ? answerMatch[1].trim() : responseText;
+
+            setMessages((prevMessages) => [...prevMessages, { sender: 'bot', text: answerText }]);
         } catch (error) {
             console.error('Error querying model:', error);
         } finally {
@@ -97,26 +89,22 @@ function App() {
                 <button onClick={handleNewChat} className="bg-indigo-600 text-white p-2 rounded-full shadow-md focus:outline-none mb-4">
                     <FiEdit />
                 </button>
-
-                {/* Rest of the sidebar content */}
                 {sidebarOpen && (
                     <div>
-                        <label className="block text-sm font-medium text-gray-300"></label>
+                        <label className="block text-sm font-medium text-gray-300">Select Model</label>
                         <select
                             value={modelType}
                             onChange={handleModelChange}
                             className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-gray-700 text-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm select-options"
                         >
                             <option value="rag">
-                                RAG 
+                                RAG
                             </option>
                             <option value="fine_tuned">
                                 Fine-tuned Model
                             </option>
-                            {/* Add more options with descriptions as needed */}
+                    
                         </select>
-
-                        {/* Welcome text and link at the bottom of the sidebar */}
                         <div className="block absolute bottom-4 left-1 right-1 text-center">
                             <FiSlack className="inline-block mr-2" />
                             <p>Welcome to Your Virtual Assistant</p>
@@ -128,22 +116,19 @@ function App() {
             </div>
             <button 
                 onClick={() => setSidebarOpen(!sidebarOpen)} 
-                // Call handleNewChat function to start a new chat
-                className="bg-indigo-990 text-white p-2 rounded-full shadow-md focus:outline-none mb-4"
+                className="bg-indigo-900 text-white p-2 rounded-full shadow-md focus:outline-none mb-4"
             >
                 <FiAlignJustify />
             </button>
-            {/* Main Content */}
             <div className="flex-1 flex flex-col items-center justify-between p-20 shadow-lg">
                 <div className="w-full max-w-4xl flex flex-col space-y-4 p-10 overflow-y-auto bg-gray-900 no-scrollbar" style={{ height: '80vh' }}>
-                    {/* Suggestions */}
                     <div className="w-full max-w-lg flex center space-x-4 mb-4">
                         {suggestions.map((suggestion, index) => (
                             <div 
                                 key={index} 
                                 onClick={() => handleSuggestionClick(suggestion)} 
-                                className="p-4 top-10 left-5 rounded-lg shadow-md bg-gray-700 text-white cursor-pointer w-full text-center relative" // Added 'relative' class for positioning
-                                style={{ Width: '100vh !important' }} // Increased maxWidth to 75%
+                                className="p-4 top-10 left-5 rounded-lg shadow-md bg-gray-700 text-white cursor-pointer w-full text-center relative"
+                                style={{ Width: '100vh !important' }}
                             >
                                 <div className="relative">
                                     {index === 0 && <FcBinoculars className="top-0 left-0 right-0 mx-auto" />}
@@ -156,7 +141,6 @@ function App() {
                             </div>
                         ))} 
                     </div>
-                    {/* Chat messages */}
                     {messages.map((msg, index) => (
                         <div 
                             key={index} 
@@ -169,7 +153,6 @@ function App() {
                     ))}
                     <div ref={messagesEndRef} />
                 </div>
-
                 <form onSubmit={handleSubmit} className="absolute bottom-0 left-0 w-full p-4  flex justify-center shadow-lg">
                     <div className="flex items-center w-full max-w-xl space-x-2" style={{ marginBottom: '40px' }}>
                         <textarea 
@@ -196,4 +179,3 @@ function App() {
 }
 
 export default App;
-
